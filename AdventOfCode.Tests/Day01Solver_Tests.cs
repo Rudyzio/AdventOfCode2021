6aa72eac251f8b104a5fcdc6f76f6e11893f33aa
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-01 22:30:33 +0000

    Solution for Day 01

--- AdventOfCode.Tests/Day01Solver_Tests.cs
+++ AdventOfCode.Tests/Day01Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_01_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day01Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day01_Input/test.input", 34241)]
-		//[InlineData("Day01_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day01Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day01_Input/test.input", 7)]
+        [InlineData("Day01_Input/puzzle.input", 1581)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day01Solver.Part1Solution(lines);
+            // Act
+            var result = Day01Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day01_Input/test.input", 51316)]
-	//[InlineData("Day01_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day01_Input/test.input", 5)]
+        [InlineData("Day01_Input/puzzle.input", 1618)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day01Solver.Part2Solution(lines);
+            // Act
+            var result = Day01Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 01 Solver/Day01Solver.cs
+++ Day 01 Solver/Day01Solver.cs
@@ -1,15 +1,50 @@
+using System;
+using System.Collections.Generic;
+
 namespace Day_01_Solver
-	{
+{
     public static class Day01Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            int[] entries = Array.ConvertAll(lines, int.Parse);
+
+            return CalculateIncreases(entries);
         }
 
         public static int Part2Solution(string[] lines)
         {
-            return 0;
+            int[] entries = Array.ConvertAll(lines, int.Parse);
+
+            var newEntries = new List<int>();
+
+            for (var i = 0; i < entries.Length; i++)
+            {
+                if (i + 1 >= entries.Length || i + 2 >= entries.Length)
+                {
+                    break;
+                }
+
+                newEntries.Add(entries[i] + entries[i + 1] + entries[i + 2]);
+            }
+
+            return CalculateIncreases(newEntries.ToArray());
+        }
+
+        private static int CalculateIncreases(int[] entries)
+        {
+            int previous = 0;
+            int increases = -1;
+            for (var i = 0; i < entries.Length; i++)
+            {
+                if (entries[i] > previous)
+                {
+                    increases++;
+                }
+                previous = entries[i];
+            }
+
+            return increases;
         }
     }
 }
