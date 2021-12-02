1c80b9a37632517d3bf6dae0f55f60afda1ccf4d
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-02 17:39:04 +0000

    Solution for Day 02

--- AdventOfCode.Tests/Day02Solver_Tests.cs
+++ AdventOfCode.Tests/Day02Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_02_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day02Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day02_Input/test.input", 34241)]
-		//[InlineData("Day02_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day02Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day02_Input/test.input", 150)]
+        [InlineData("Day02_Input/puzzle.input", 1989265)]
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day02Solver.Part1Solution(lines);
+            // Act
+            var result = Day02Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day02_Input/test.input", 51316)]
-	//[InlineData("Day02_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day02_Input/test.input", 900)]
+        [InlineData("Day02_Input/puzzle.input", 2089174012)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day02Solver.Part2Solution(lines);
+            // Act
+            var result = Day02Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 02 Solver/Day02Solver.cs
+++ Day 02 Solver/Day02Solver.cs
@@ -1,15 +1,63 @@
 namespace Day_02_Solver
-	{
+{
     public static class Day02Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            int x = 0;
+            int depth = 0;
+            foreach (var line in lines)
+            {
+                var splitted = line.Split(" ");
+                var command = splitted[0];
+                var value = int.Parse(splitted[1]);
+
+                switch (command)
+                {
+                    case "forward":
+                        x += value;
+                        break;
+                    case "down":
+                        depth += value;
+                        break;
+                    case "up":
+                        depth -= value;
+                        break;
+                }
+            }
+
+            return x * depth;
         }
 
         public static int Part2Solution(string[] lines)
         {
-            return 0;
+            int x = 0;
+            int depth = 0;
+            int aim = 0;
+            foreach (var line in lines)
+            {
+                // System.Console.WriteLine($"Command: {line}");
+                var splitted = line.Split(" ");
+                var command = splitted[0];
+                var value = int.Parse(splitted[1]);
+
+                switch (command)
+                {
+                    case "forward":
+                        x += value;
+                        depth += (aim * value);
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                }
+                // System.Console.WriteLine($"X:{x} ; Depth:{depth} ; Aim:{aim}");
+            }
+
+            return x * depth;
         }
     }
 }
