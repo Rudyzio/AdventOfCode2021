0960bf6afb3f7c4946d013444570c8308319f510
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-04 18:49:07 +0000

    Solution for Day 03

--- AdventOfCode.Tests/Day03Solver_Tests.cs
+++ AdventOfCode.Tests/Day03Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_03_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day03Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day03_Input/test.input", 34241)]
-		//[InlineData("Day03_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day03Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day03_Input/test.input", 198)]
+        [InlineData("Day03_Input/puzzle.input", 749376)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day03Solver.Part1Solution(lines);
+            // Act
+            var result = Day03Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day03_Input/test.input", 51316)]
-	//[InlineData("Day03_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day03_Input/test.input", 230)]
+        [InlineData("Day03_Input/puzzle.input", 2372923)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day03Solver.Part2Solution(lines);
+            // Act
+            var result = Day03Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 03 Solver/Day03Solver.cs
+++ Day 03 Solver/Day03Solver.cs
@@ -1,15 +1,145 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace Day_03_Solver
-	{
+{
     public static class Day03Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            (string gammaRate, string epsilonRate) = GetRates(lines);
+
+            return Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
         }
 
         public static int Part2Solution(string[] lines)
         {
-            return 0;
+            var lineLength = lines[0].Length;
+
+            var oxygenRating = new List<string>(lines);
+            var co2Rating = new List<string>(lines);
+            var index = 0;
+
+            while (true)
+            {
+                if (oxygenRating.Count == 1 && co2Rating.Count == 1)
+                {
+                    break;
+                }
+
+                if (oxygenRating.Count > 1)
+                {
+                    var mostCommonOxygen = GetMostCommonCount(oxygenRating, index);
+                    oxygenRating = GetWithOccurrenceAtIndex(oxygenRating, index, mostCommonOxygen);
+                }
+
+                if (co2Rating.Count > 1)
+                {
+                    var leastCommonCo2 = GetLeastCommonCount(co2Rating, index);
+                    co2Rating = GetWithOccurrenceAtIndex(co2Rating, index, leastCommonCo2);
+                }
+
+                index++;
+            }
+
+            return Convert.ToInt32(oxygenRating.First(), 2) * Convert.ToInt32(co2Rating.First(), 2);
+        }
+
+        private static (string gammaRate, string epsilonRate) GetRates(string[] lines)
+        {
+            int totalLines = lines.Length;
+            int[] gammaRateCounter = new int[lines[0].Length];
+            StringBuilder gammaRate = new();
+            StringBuilder epsilonRate = new();
+
+            foreach (var line in lines)
+            {
+                for (var i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '1')
+                    {
+                        gammaRateCounter[i]++;
+                    }
+                }
+            }
+
+            for (var i = 0; i < gammaRateCounter.Length; i++)
+            {
+                if (gammaRateCounter[i] > totalLines / 2)
+                {
+                    gammaRate.Append('1');
+                    epsilonRate.Append('0');
+                }
+                else
+                {
+                    gammaRate.Append('0');
+                    epsilonRate.Append('1');
+                }
+            }
+
+            return (gammaRate.ToString(), epsilonRate.ToString());
+        }
+
+        private static char GetMostCommonCount(List<string> lines, int index)
+        {
+            double totalLines = lines.Count;
+            double countObjective = 0;
+
+            foreach (var line in lines)
+            {
+                if (line[index] == '1')
+                {
+                    countObjective++;
+                }
+            }
+
+            if (countObjective >= totalLines / 2)
+            {
+                return '1';
+            }
+            else
+            {
+                return '0';
+            }
+        }
+
+        private static char GetLeastCommonCount(List<string> lines, int index)
+        {
+            var commonCount = GetMostCommonCount(lines, index);
+            if (commonCount == '1')
+            {
+                return '0';
+            }
+            else
+            {
+                return '1';
+            }
+        }
+
+        private static List<string> GetWithOccurrenceAtIndex(List<string> lines, int index, char bit)
+        {
+            var toReturn = new List<string>();
+
+            foreach (var line in lines)
+            {
+                if (line[index] == bit)
+                {
+                    toReturn.Add(line);
+                }
+            }
+
+            return toReturn;
+        }
+
+        private static void Print(this List<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                System.Console.WriteLine(line);
+            }
+            System.Console.WriteLine("------------------------------");
         }
     }
 }
