1ec842f5e1ef2f70de3b0be0515fd233fbb09f92
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-07 19:49:57 +0000

    Solution for day 06

--- AdventOfCode.Tests/Day06Solver_Tests.cs
+++ AdventOfCode.Tests/Day06Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_06_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day06Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day06_Input/test.input", 34241)]
-		//[InlineData("Day06_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day06Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day06_Input/test.input", 5934)]
+        [InlineData("Day06_Input/puzzle.input", 373378)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day06Solver.Part1Solution(lines);
+            // Act
+            var result = Day06Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day06_Input/test.input", 51316)]
-	//[InlineData("Day06_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day06_Input/test.input", 26984457539)]
+        [InlineData("Day06_Input/puzzle.input", 1682576647495)]
+        public void TestPart2Solution(string inputFile, long expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day06Solver.Part2Solution(lines);
+            // Act
+            var result = Day06Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 06 Solver/Day06Solver.cs
+++ Day 06 Solver/Day06Solver.cs
@@ -1,15 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Day_06_Solver
-	{
+{
     public static class Day06Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            int currentDay = 1;
+            int days = 80;
+
+            var currentLanternfishes = ParseInput(lines);
+
+            while (currentDay <= days)
+            {
+                var nextLanterfishes = new List<long>();
+                var appendToEnd = new List<long>();
+                foreach (var fish in currentLanternfishes)
+                {
+                    if (fish == 0)
+                    {
+                        appendToEnd.Add(8);
+                        nextLanterfishes.Add(6);
+                    }
+                    else
+                    {
+                        nextLanterfishes.Add(fish - 1);
+                    }
+                }
+                currentLanternfishes = nextLanterfishes.Concat(appendToEnd).ToList();
+                // currentLanternfishes.Print(currentDay);
+                currentDay++;
+            }
+            return currentLanternfishes.Count();
         }
 
-        public static int Part2Solution(string[] lines)
+        public static long Part2Solution(string[] lines)
         {
-            return 0;
+            int currentDay = 1;
+            int days = 256;
+
+            var currentLanternfishes = ParseInput(lines);
+            long[] fishGeneration = new long[9];
+            foreach (int i in currentLanternfishes)
+            {
+                fishGeneration[i]++;
+            }
+
+            while (currentDay <= days)
+            {
+                long newFishToday = fishGeneration[0];
+                for (int i = 1; i < fishGeneration.Length; i++)
+                {
+                    fishGeneration[i - 1] = fishGeneration[i];
+                }
+
+                fishGeneration[8] = newFishToday;
+                fishGeneration[6] += newFishToday;
+
+                // fishGeneration.Print(currentDay);
+
+                currentDay++;
+            }
+
+            return fishGeneration.Sum();
+        }
+
+        private static List<long> ParseInput(string[] lines)
+        {
+            return lines[0].Split(",").Select<string, long>(x => int.Parse(x)).ToList();
+        }
+
+        private static void Print(this long[] fishes, int days)
+        {
+            System.Console.WriteLine($"After {days} days: {string.Join(",", fishes)}");
+        }
+
+        private static void Print(this List<long> fishes, int days)
+        {
+            System.Console.WriteLine($"After {days} days: {string.Join(",", fishes.ToArray())}");
         }
     }
 }
