1bdc891366b1d0d842a9c23a0bbb357308ef18ad
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-07 22:31:39 +0000

    Solution for Day 07

--- AdventOfCode.Tests/Day07Solver_Tests.cs
+++ AdventOfCode.Tests/Day07Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_07_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day07Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day07_Input/test.input", 34241)]
-		//[InlineData("Day07_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day07Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day07_Input/test.input", 37)]
+        [InlineData("Day07_Input/puzzle.input", 351901)]	
+        public void TestPart1Solution(string inputFile, long expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day07Solver.Part1Solution(lines);
+            // Act
+            var result = Day07Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day07_Input/test.input", 51316)]
-	//[InlineData("Day07_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day07_Input/test.input", 168)]
+        [InlineData("Day07_Input/puzzle.input", 101079875)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day07Solver.Part2Solution(lines);
+            // Act
+            var result = Day07Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 07 Solver/Day07Solver.cs
+++ Day 07 Solver/Day07Solver.cs
@@ -1,15 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Day_07_Solver
-	{
+{
     public static class Day07Solver
     {
-        public static int Part1Solution(string[] lines)
+        public static long Part1Solution(string[] lines)
         {
-            return 0;
+            var positions = ParseInput(lines);
+            // positions.Print();
+            positions = positions.OrderBy(x => x).ToList();
+            // positions.Print();
+
+            var totalCrabs = positions.Count();
+            double middle = totalCrabs / 2;
+            long median = 0;
+
+            if (IsEven(totalCrabs))
+            {
+                median = (positions[(int)Math.Ceiling(middle)] + positions[(int)Math.Round(middle)]) / 2;
+            }
+            else
+            {
+                median = positions[totalCrabs / 2];
+            }
+            // System.Console.WriteLine($"{median}");
+
+            long toReturn = 0;
+
+            foreach (var pos in positions)
+            {
+                toReturn += Math.Abs(median - pos);
+            }
+
+            return toReturn;
         }
 
-        public static int Part2Solution(string[] lines)
+        public static long Part2Solution(string[] lines)
         {
-            return 0;
+            var positions = ParseInput(lines);
+
+            var totalCrabs = positions.Count();
+            double averageCeiling = Math.Round(positions.Average());
+            double averageFloor = Math.Floor(positions.Average());
+
+            long toReturnCeiling = 0;
+            long toReturnFloor = 0;
+
+            foreach (var pos in positions)
+            {
+                toReturnCeiling += CalculateDigitSum((long)Math.Abs(pos - averageCeiling));
+                toReturnFloor += CalculateDigitSum((long)Math.Abs(pos - averageFloor));
+            }
+
+            return toReturnCeiling < toReturnFloor ? toReturnCeiling : toReturnFloor;
+        }
+
+        private static List<long> ParseInput(string[] lines)
+        {
+            return lines[0].Split(",").Select<string, long>(x => int.Parse(x)).ToList();
+        }
+
+        private static bool IsEven(long number)
+        {
+            return number % 2 == 0;
+        }
+
+        private static void Print(this List<long> lines)
+        {
+            System.Console.WriteLine($"{string.Join(",", lines.ToArray())}");
+        }
+
+        private static long CalculateDigitSum(long n)
+        {
+            return n * (n + 1) / 2;
         }
     }
 }
