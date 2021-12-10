2e9b82b71e7721a68972d32ee3551cb658e9109d
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-10 23:55:34 +0000

    Solution for Day 09

--- AdventOfCode.Tests/Day09Solver_Tests.cs
+++ AdventOfCode.Tests/Day09Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_09_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day09Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day09_Input/test.input", 34241)]
-		//[InlineData("Day09_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day09Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day09_Input/test.input", 15)]
+        [InlineData("Day09_Input/puzzle.input", 496)]
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day09Solver.Part1Solution(lines);
+            // Act
+            var result = Day09Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day09_Input/test.input", 51316)]
-	//[InlineData("Day09_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day09_Input/test.input", 1134)]
+        [InlineData("Day09_Input/puzzle.input", 902880)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day09Solver.Part2Solution(lines);
+            // Act
+            var result = Day09Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 09 Solver/Day09Solver.cs
+++ Day 09 Solver/Day09Solver.cs
@@ -1,15 +1,166 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Day_09_Solver
-	{
+{
     public static class Day09Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            var heightMap = ReadInput(lines);
+            var toReturn = 0;
+
+            for (var i = 0; i < heightMap.Length; i++)
+            {
+                for (var j = 0; j < heightMap[i].Length; j++)
+                {
+                    var adjacent = heightMap.GetAdjacent(i, j);
+                    if (adjacent.All(x => x > heightMap[i][j]))
+                    {
+                        toReturn += heightMap[i][j] + 1;
+                    }
+                }
+            }
+
+            return toReturn;
         }
 
         public static int Part2Solution(string[] lines)
         {
-            return 0;
+            var heightMap = ReadInput(lines);
+            var basins = new List<int>();
+
+            for (var i = 0; i < heightMap.Length; i++)
+            {
+                for (var j = 0; j < heightMap[i].Length; j++)
+                {
+                    var adjacent = heightMap.GetAdjacentWithPosition(i, j);
+                    if (adjacent.All(x => x.Value > heightMap[i][j]))
+                    {
+                        var basinSize = CalculateBasinSize(heightMap, i, j);
+                        // System.Console.WriteLine("Basin size: " + basinSize);
+                        basins.Add(basinSize);
+                    }
+                }
+            }
+
+            var toReturn = 1;
+            foreach (var basin in basins.OrderByDescending(x => x).Take(3))
+            {
+                // System.Console.WriteLine(basin);
+                toReturn *= basin;
+            }
+
+            return toReturn;
+        }
+
+        private static int CalculateBasinSize(int[][] heightMap, int x, int y)
+        {
+            var queue = new Queue<(KeyValuePair<int, int> Position, int Value)>();
+            queue.Enqueue((new KeyValuePair<int, int>(x, y), heightMap[x][y]));
+            var basin = new List<KeyValuePair<int, int>> { new KeyValuePair<int, int>(x, y) };
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                var adjacent = heightMap.GetAdjacentWithPosition(current.Position.Key, current.Position.Value);
+
+                // System.Console.WriteLine($"Current value: {current.Value}");
+                foreach (var adj in adjacent)
+                {
+                    if (basin.Any(x => x.Key == adj.Position.Key && x.Value == adj.Position.Value))
+                    {
+                        continue;
+                    }
+
+                    if (adj.Value < 9 && current.Value < adj.Value)
+                    {
+                        // System.Console.WriteLine($"Adjacent: {adj.Value}");
+                        queue.Enqueue(adj);
+                        basin.Add(adj.Position);
+                    }
+                }
+            }
+
+            return basin.Count();
+        }
+
+        private static int[][] ReadInput(string[] lines)
+        {
+            var heightMap = new int[lines.Length][];
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                heightMap[i] = new int[lines[i].Length];
+                for (var j = 0; j < lines[i].Length; j++)
+                {
+                    heightMap[i][j] = int.Parse(lines[i][j].ToString());
+                }
+            }
+
+            return heightMap;
+        }
+
+        private static List<int> GetAdjacent(this int[][] heightMap, int x, int y)
+        {
+            List<int> adjacentList = new();
+
+            if (x > 0)
+            {
+                // Above
+                adjacentList.Add(heightMap[x - 1][y]);
+            }
+
+            if (y > 0)
+            {
+                // Left
+                adjacentList.Add(heightMap[x][y - 1]);
+            }
+
+            if (y + 1 < heightMap[0].Length)
+            {
+                // Left
+                adjacentList.Add(heightMap[x][y + 1]);
+            }
+
+            if (x + 1 < heightMap.Length)
+            {
+                // Left
+                adjacentList.Add(heightMap[x + 1][y]);
+            }
+
+            return adjacentList;
+        }
+
+        private static List<(KeyValuePair<int, int> Position, int Value)> GetAdjacentWithPosition(this int[][] heightMap, int x, int y)
+        {
+            List<(KeyValuePair<int, int>, int)> adjacentList = new();
+
+            if (x > 0)
+            {
+                // Above
+                adjacentList.Add((new KeyValuePair<int, int>(x - 1, y), heightMap[x - 1][y]));
+            }
+
+            if (y > 0)
+            {
+                // Left
+                adjacentList.Add((new KeyValuePair<int, int>(x, y - 1), heightMap[x][y - 1]));
+            }
+
+            if (y + 1 < heightMap[0].Length)
+            {
+                // Left
+                adjacentList.Add((new KeyValuePair<int, int>(x, y + 1), heightMap[x][y + 1]));
+            }
+
+            if (x + 1 < heightMap.Length)
+            {
+                // Left
+                adjacentList.Add((new KeyValuePair<int, int>(x + 1, y), heightMap[x + 1][y]));
+            }
+
+            return adjacentList;
         }
     }
 }
