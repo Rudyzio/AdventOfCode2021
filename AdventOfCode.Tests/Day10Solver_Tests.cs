aa81beea546419640b63259f8822a59460f69d9e
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-11 00:31:55 +0000

    Solution for Day 10

--- AdventOfCode.Tests/Day10Solver_Tests.cs
+++ AdventOfCode.Tests/Day10Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_10_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day10Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day10_Input/test.input", 34241)]
-		//[InlineData("Day10_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day10Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day10_Input/test.input", 26397)]
+        [InlineData("Day10_Input/puzzle.input", 462693)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day10Solver.Part1Solution(lines);
+            // Act
+            var result = Day10Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day10_Input/test.input", 51316)]
-	//[InlineData("Day10_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day10_Input/test.input", 288957)]
+        [InlineData("Day10_Input/puzzle.input", 3094671161)]
+        public void TestPart2Solution(string inputFile, long expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day10Solver.Part2Solution(lines);
+            // Act
+            var result = Day10Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 10 Solver/Day10Solver.cs
+++ Day 10 Solver/Day10Solver.cs
@@ -1,15 +1,122 @@
+using System.Collections.Generic;
+
 namespace Day_10_Solver
 {
     public static class Day10Solver
     {
-        public static int Part1Solution(string[] lines)
+        public static long Part1Solution(string[] lines)
         {
-            return 0;
+            List<char> illegals = new();
+            foreach (var line in lines)
+            {
+                (var hasError, var stack) = CheckInput(line, illegals);
+            }
+
+            var toReturn = 0;
+            foreach (var illegal in illegals)
+            {
+                switch (illegal)
+                {
+                    case ')':
+                        toReturn += 3;
+                        break;
+                    case ']':
+                        toReturn += 57;
+                        break;
+                    case '}':
+                        toReturn += 1197;
+                        break;
+                    case '>':
+                        toReturn += 25137;
+                        break;
+                }
+            }
+
+            return toReturn;
         }
 
-        public static int Part2Solution(string[] lines)
+        public static long Part2Solution(string[] lines)
         {
-            return 0;
+            var incompleteScores = new List<long>();
+
+            foreach (var line in lines)
+            {
+                (var foundError, var stack) = CheckInput(line);
+
+                if (!foundError)
+                {
+                    long score = 0;
+                    while (stack.Count > 0)
+                    {
+                        var current = stack.Pop();
+                        switch (current)
+                        {
+                            case ')':
+                                score = score * 5 + 1;
+                                break;
+                            case ']':
+                                score = score * 5 + 2;
+                                break;
+                            case '}':
+                                score = score * 5 + 3;
+                                break;
+                            case '>':
+                                score = score * 5 + 4;
+                                break;
+                        }
+                    }
+
+                    if (score > 0)
+                        incompleteScores.Add(score);
+                }
+            }
+
+            incompleteScores.Sort();
+            return incompleteScores[incompleteScores.Count / 2];
+        }
+
+        private static (bool, Stack<char>) CheckInput(string line, List<char> illegals = null)
+        {
+            var stack = new Stack<char>();
+            bool foundError = false;
+            foreach (var chunk in line)
+            {
+                switch (chunk)
+                {
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    case '<':
+                        stack.Push('>');
+                        break;
+
+                    case ')':
+                    case ']':
+                    case '}':
+                    case '>':
+                        var closing = stack.Pop();
+                        if (closing != chunk)
+                        {
+                            foundError = true;
+                            if (illegals != null)
+                                illegals.Add(chunk);
+                        }
+                        break;
+                }
+
+                if (foundError)
+                {
+                    break;
+                }
+            }
+
+            return (foundError, stack);
         }
     }
 }
