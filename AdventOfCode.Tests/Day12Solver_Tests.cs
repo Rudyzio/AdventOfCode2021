ef6b2f909e005e6939fc421fb1fc326c14d8e620
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-12 12:11:34 +0000

    Solution for Day 12

--- AdventOfCode.Tests/Day12Solver_Tests.cs
+++ AdventOfCode.Tests/Day12Solver_Tests.cs
@@ -2,36 +2,40 @@ using Day_12_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day12Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day12_Input/test.input", 34241)]
-		//[InlineData("Day12_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day12Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day12_Input/test1.input", 10)]
+        [InlineData("Day12_Input/test2.input", 19)]
+        [InlineData("Day12_Input/test3.input", 226)]
+        [InlineData("Day12_Input/puzzle.input", 3802)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day12Solver.Part1Solution(lines);
+            // Act
+            var result = Day12Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day12_Input/test.input", 51316)]
-	//[InlineData("Day12_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day12_Input/test1.input", 36)]
+        [InlineData("Day12_Input/test2.input", 103)]
+        [InlineData("Day12_Input/test3.input", 3509)]
+        [InlineData("Day12_Input/puzzle.input", 99448)]
+        public void TestPart2Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day12Solver.Part2Solution(lines);
+            // Act
+            var result = Day12Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 12 Solver/Day12Solver.cs
+++ Day 12 Solver/Day12Solver.cs
@@ -1,15 +1,135 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Day_12_Solver
 {
     public static class Day12Solver
     {
         public static int Part1Solution(string[] lines)
         {
-            return 0;
+            var paths = ParseInput(lines);
+            var toReturn = 0;
+
+            FindPathsAtLeastOnceSmallCaves(paths, "start", "start", ref toReturn);
+
+            return toReturn;
         }
 
         public static int Part2Solution(string[] lines)
         {
-            return 0;
+            var paths = ParseInput(lines);
+            var toReturn = 0;
+
+            FindPathsAtLeastTwiceOneSmallCaves(paths, "start", "start", ref toReturn);
+
+            return toReturn;
+        }
+
+        private static IDictionary<string, List<string>> ParseInput(string[] lines)
+        {
+            var toReturn = new Dictionary<string, List<string>>();
+
+            foreach (var line in lines)
+            {
+                var startPoint = line.Split("-")[0];
+                var endPoint = line.Split("-")[1];
+
+                if (!toReturn.ContainsKey(startPoint))
+                {
+                    toReturn.Add(startPoint, new List<string>() { endPoint });
+                }
+                else
+                {
+                    toReturn[startPoint].Add(endPoint);
+                }
+
+                if (!toReturn.ContainsKey(endPoint))
+                {
+                    toReturn.Add(endPoint, new List<string>() { startPoint });
+                }
+                else
+                {
+                    toReturn[endPoint].Add(startPoint);
+                }
+            }
+
+            return toReturn;
+        }
+
+        private static bool IsUppercase(this string input)
+        {
+            return input.All(x => char.IsUpper(x));
+        }
+
+        private static void FindPathsAtLeastOnceSmallCaves(IDictionary<string, List<string>> paths, string currentPath, string currentNode, ref int count)
+        {
+            if (currentNode.Equals("end"))
+            {
+                count++;
+                return;
+            }
+
+            var children = paths[currentNode];
+
+            foreach (var child in children)
+            {
+                if ((child.Equals("start")) || (currentPath.Contains(child) && !child.IsUppercase() && !child.Equals("end")))
+                {
+                    continue;
+                }
+                else
+                {
+                    FindPathsAtLeastOnceSmallCaves(paths, currentPath + child, child, ref count);
+                }
+            }
+        }
+
+        private static void FindPathsAtLeastTwiceOneSmallCaves(IDictionary<string, List<string>> paths, string currentPath, string currentNode, ref int count, bool visitedTwice = false)
+        {
+            if (currentNode.Equals("end"))
+            {
+                count++;
+                return;
+            }
+
+            var children = paths[currentNode];
+            var alreadyVisitedTwice = false;
+            if (visitedTwice)
+                alreadyVisitedTwice = true;
+            foreach (var child in children)
+            {
+                if (
+                    child.Equals("start") ||
+                    (currentPath.Contains(child) && !child.IsUppercase() && !child.Equals("end") && alreadyVisitedTwice)
+                )
+                {
+                    continue;
+                }
+                else
+                {
+                    if (!alreadyVisitedTwice)
+                    {
+                        if (currentPath.Contains(child) && !child.IsUppercase())
+                        {
+                            visitedTwice = true;
+                        }
+                        else
+                        {
+                            visitedTwice = false;
+                        }
+                    }
+
+                    FindPathsAtLeastTwiceOneSmallCaves(paths, currentPath + "," + child, child, ref count, visitedTwice);
+                }
+            }
+        }
+
+        private static void Print(this IDictionary<string, List<string>> input)
+        {
+            foreach (var entry in input)
+            {
+                System.Console.WriteLine($"{entry.Key} --> {string.Join(",", entry.Value)}");
+            }
         }
     }
 }
