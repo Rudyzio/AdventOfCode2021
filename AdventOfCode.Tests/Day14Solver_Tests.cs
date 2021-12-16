124f5572ef7fa72c67ef8809eef19c0bbb80f1b6
Rodrigo Santos <rudyzio92@gmail.com>
2021-12-16 00:55:00 +0000

    Solution for Day 14

--- AdventOfCode.Tests/Day14Solver_Tests.cs
+++ AdventOfCode.Tests/Day14Solver_Tests.cs
@@ -2,36 +2,36 @@ using Day_14_Solver;
 using Xunit;
 namespace AdventOfCode.Tests
 {
-   public class Day14Solver_Tests
-   {
-		//[Theory]
-		//[InlineData("Day14_Input/test.input", 34241)]
-		//[InlineData("Day14_Input/puzzle.input", 3429947)]	
-       public void TestPart1Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+    public class Day14Solver_Tests
+    {
+        [Theory]
+        [InlineData("Day14_Input/test.input", 1588)]
+        [InlineData("Day14_Input/puzzle.input", 3259)]	
+        public void TestPart1Solution(string inputFile, int expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day14Solver.Part1Solution(lines);
+            // Act
+            var result = Day14Solver.Part1Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
+            // Assert
+            Assert.Equal(expected, result);
+        }
 
-		//[Theory]
-		//[InlineData("Day14_Input/test.input", 51316)]
-	//[InlineData("Day14_Input/puzzle.input", 5142043)]
-       public void TestPart2Solution(string inputFile, int expected)
-       {
-			// Arrange
-			string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
+        [Theory]
+        [InlineData("Day14_Input/test.input", 2188189693529)]
+        [InlineData("Day14_Input/puzzle.input", 3459174981021)]
+        public void TestPart2Solution(string inputFile, long expected)
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines($"../../../{inputFile}");
 
-			// Act
-			var result = Day14Solver.Part2Solution(lines);
+            // Act
+            var result = Day14Solver.Part2Solution(lines);
 
-			// Assert
-			Assert.Equal(expected, result);
-       }
-   }
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
 }
--- Day 14 Solver/Day14Solver.cs
+++ Day 14 Solver/Day14Solver.cs
@@ -1,15 +1,130 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace Day_14_Solver
 {
     public static class Day14Solver
     {
-        public static int Part1Solution(string[] lines)
+        public static long Part1Solution(string[] lines)
         {
-            return 0;
+            (var polymerTemplate, var pairInsertionRules) = ParseInput(lines);
+
+            return Steps(10, polymerTemplate, pairInsertionRules);
         }
 
-        public static int Part2Solution(string[] lines)
+        public static long Part2Solution(string[] lines)
         {
-            return 0;
+            (var polymerTemplate, var pairInsertionRules) = ParseInput(lines);
+
+            return Steps(40, polymerTemplate, pairInsertionRules);
         }
+
+        private static long Steps(int steps, string polymerTemplate, Dictionary<string, string> pairInsertionRules)
+        {
+            var dictionary = new Dictionary<string, long>();
+            var charCount = new Dictionary<string, long>();
+
+            for (var i = 0; i < polymerTemplate.Length; i++)
+            {
+                charCount.AddIfNotContains(polymerTemplate[i].ToString(), 1);
+
+                if (i + 1 >= polymerTemplate.Length) continue;
+                dictionary.AddIfNotContains(polymerTemplate.Substring(i, 2), 1);
+            }
+
+            int step = 0;
+            while (step < steps)
+            {
+                var bufferDict = new Dictionary<string, long>(dictionary);
+
+                foreach (var rule in pairInsertionRules)
+                {
+                    if (!dictionary.ContainsKey(rule.Key))
+                    {
+                        continue;
+                    }
+
+                    bufferDict[rule.Key] -= dictionary[rule.Key];
+                    var firstPair = rule.Key[0] + rule.Value;
+                    var secondPair = rule.Value + rule.Key[1];
+
+                    bufferDict.AddIfNotContains(firstPair, dictionary[rule.Key]);
+                    bufferDict.AddIfNotContains(secondPair, dictionary[rule.Key]);
+                    charCount.AddIfNotContains(rule.Value, dictionary[rule.Key]);
+                }
+                dictionary = new Dictionary<string, long>(bufferDict);
+
+                step++;
+            }
+
+            return charCount.Values.Max() - charCount.Values.Min();
+        }
+
+        // private static string Step(string polymerTemplate, Dictionary<string, string> pairs)
+        // {
+        //     StringBuilder toReturn = new StringBuilder();
+
+        //     for (var i = 0; i < polymerTemplate.Length - 1; i++)
+        //     {
+        //         string pair = polymerTemplate[i].ToString() + polymerTemplate[i + 1].ToString();
+        //         var adjacent = pairs[pair];
+
+        //         if (string.IsNullOrEmpty(toReturn.ToString()))
+        //         {
+        //             toReturn.Append(polymerTemplate[i].ToString());
+        //         }
+
+        //         toReturn.Append(adjacent);
+        //         toReturn.Append(polymerTemplate[i + 1].ToString());
+        //     }
+
+        //     return toReturn.ToString();
+        // }
+
+        private static (string, Dictionary<string, string>) ParseInput(string[] lines)
+        {
+            var polymerTemplate = string.Empty;
+            var pairInsertionRules = new Dictionary<string, string>();
+
+            bool insertionRules = false;
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    insertionRules = true;
+                    continue;
+                }
+
+                if (insertionRules)
+                {
+                    var pair = line.Split("->")[0].Trim();
+                    var adjacent = line.Split("->")[1].Trim();
+                    pairInsertionRules.Add(pair, adjacent);
+                }
+                else
+                {
+                    polymerTemplate = line;
+                }
+            }
+
+            return (polymerTemplate, pairInsertionRules);
+        }
+
+        private static void AddIfNotContains(this Dictionary<string, long> dict, string key, long value)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                dict.Add(key, 0);
+            }
+
+            dict[key] += value;
+        }
+    }
+
+    public class Pair
+    {
+        public string First { get; set; }
+        public string Second { get; set; }
     }
 }
